1891328268fd50a973c269bbccf18d74808bf9b3
Kostya (AgapovKS) <alice@example.com>
2021-07-07 16:28:02 +0300

    code: вычесление минмакс

--- histogram.cpp
+++ histogram.cpp
@@ -19,22 +19,6 @@ void find_minmax(const vector<double>& numbers, double& min, double& max)
     }
 }
 
-double domashnee(int IMAGE_WIDTH, int BLOCK_WIDTH, size_t number_count)
-{
-    while (IMAGE_WIDTH<70 || IMAGE_WIDTH>800 ||IMAGE_WIDTH<(number_count/3.0*BLOCK_WIDTH))
-    {
-        if (IMAGE_WIDTH<70)
-            cout<<"Vvedite shirinu zanovo, tk ona menshe 70. ";
-        if (IMAGE_WIDTH>800)
-            cout<<"Vvedite shirinu zanovo, tk ona bolshe 800. ";
-        if (IMAGE_WIDTH<(number_count/3.0*BLOCK_WIDTH))
-            cout<<"Vvedite shirinu zanovo, tk ona menshe treti kolichestva, umnozhennih na BLOCK_WIDTH. ";
-        cout<<"Novoe znachenie shirini: ";
-        cin>>IMAGE_WIDTH;
-    }
-    return IMAGE_WIDTH;
-}
-
 
 
 
