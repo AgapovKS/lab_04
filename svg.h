4cf7be07a4f18c17600dc618df0b84c82e260354
Kostya (AgapovKS) <alice@example.com>
2021-07-08 00:23:13 +0300

    code: заголовок

--- svg.h
+++ svg.h
@@ -8,10 +8,8 @@
 #include <windows.h>
 using namespace std;
 using namespace std;
-void
-svg_begin(double width, double height);
-void
-svg_text(double left, double baseline, string text);
+void svg_begin(double width, double height);
+void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height,string stroke, string fill);
 void
 svg_end();
