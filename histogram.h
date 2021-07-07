50be3fd1fd396276c13b08ad44ab669e2373d8a9
Kostya (AgapovKS) <alice@example.com>
2021-07-07 16:28:44 +0300

    code: заголовок

--- histogram.h
+++ histogram.h
@@ -10,11 +10,6 @@ struct Input
     size_t bin_count;
 };
 void find_minmax(const vector<double>& numbers, double& min, double& max);
-void show_histogram_svg(const vector<size_t>& bins, size_t number_count);
-void svg_end();
-void svg_begin(double width, double height);
-void svg_rect(double x, double y, double width, double height,string stroke, string fill);
-void domashnee(int IMAGE_WIDTH, int BLOCK_WIDTH, size_t number_count);
-void svg_text(double left, double baseline, string text);
+
 #endif // HISTOGRAM_H_INCLUDED
 
